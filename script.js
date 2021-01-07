fe5cf51de65c83239e7691c26de16b9a5da7fa7a
fabianoSAraujo <araujo.s.fabiano@gmail.com>
2021-01-07 17:37:49 +0000

    fim do projeto

--- script.js
+++ script.js
@@ -1,10 +1,92 @@
 'use strict';
 
+// Selecionando os Elementos
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
-// Condições Iniciais
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  // Condições Iniciais
+
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--winner');
+};
+init();
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+// Funcionalidade: Rolar Dados
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    //1. Gerar um dado randomico
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    //2. Mostrar dado
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+    // 3. Verifica se o dado é igual a 1
+    if (dice !== 1) {
+      // Soma para o placar corrente
+      currentScore += dice;
+      document.getElementById(
+        `current--${activePlayer}`
+      ).textContent = currentScore;
+    } else {
+      //Mudar de jogador
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // Adiciona os pontos correntes para o jogador do momento
+    scores[activePlayer] += currentScore;
+    // scores[1] = scores[1] + currentScore
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // Verfica se o jogador atingiu 100 pontos
+    if (scores[activePlayer] >= 100) {
+      // Fim de jogo
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      //Trocar de Jogador
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', init);
